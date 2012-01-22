f865a792ea2beefcd8f122d8bdb51ceec69970f2
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-22 22:32:56 +1100

    Updated binary to call Command as Thor app

---
+++ lib/test_pilot/command.rb
@@ -0,0 +1,16 @@
+require "thor"
+
+module TestPilot
+  class Command < Thor
+    
+    include TestPilot::Helpers
+
+    desc :setup, "Setup my account so testpilot can authenticate"
+    def setup
+      display_header "Authentication"
+      TestPilot::Auth.login
+      output_with_arrow "Successful."
+    end
+
+  end
+end
