f3262272a1f2f1b440fecd4fc142b31b610db579
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-22 22:34:05 +1100

    Autoload dependencies

--- lib/test_pilot.rb
+++ lib/test_pilot.rb
@@ -1,4 +1,8 @@
 require "test_pilot/version"
 
 module TestPilot
+  autoload :Command,  'test_pilot/command'
+  autoload :Auth,     'test_pilot/auth'
+  autoload :Helpers,  'test_pilot/helpers'
+
 end
