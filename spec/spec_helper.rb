9d0369d9a5fa340d5f1fb492dcc9d605211dbdbd
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-22 22:32:05 +1100

    Prepared spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,29 @@
-$: << 'lib'
+$stdin = File.new("/dev/null")
+
+require "rubygems"
+require "bundler/setup"
+
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+require "rspec"
+require "fakefs/safe"
+require 'tmpdir'
+require "webmock/rspec"
+
 require "test_pilot"
 
 Dir[File.expand_path("../support/*.rb", __FILE__)].each { |file| require file }
+
+require "test_pilot/helpers"
+module TestPilot::Helpers
+  undef_method :home_directory
+  def home_directory
+    @home_directory ||= Dir.mktmpdir
+  end
+end
 
 RSpec.configure do |config|
   config.mock_with :rspec
